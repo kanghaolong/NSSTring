c9d1f07b42de0aae63354691ea11c3421a5a4391
lkjy <lkjy@kanghaolongdemac-mini.local>
2016-02-29 20:19:55 +0800

    kanghaolong

---
+++ NSSTring/NSSTring/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSSTring
+//
+//  Created by lkjy on 16/2/29.
+//  Copyright © 2016年 康康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSSTring/NSSTring/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NSSTring
+//
+//  Created by lkjy on 16/2/29.
+//  Copyright © 2016年 康康. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
